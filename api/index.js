015e3ad1511bd0b86417092678294e73f0e60356
pranaymalap1483 <pranay.p.malap@gmail.com>
2023-12-22 19:30:03 +0530

    new update

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: " http://localhost:5173",
+    origin: " https://lively-marzipan-5d2ead.netlify.app",
   })
 );
 app.use("/uploads", express.static(__dirname + "/uploads"));
